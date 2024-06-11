96ba35d1edf5658ac5a5f5e029d1e748b442f327
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-11 15:28:36 +0900

    feat: 로그인 및 회원가입 페이지 css 적용

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
+            screens: {
+                "max-md": { max: "768px" },
+                "max-lg": { max: "1024px" },
+                "max-570": { max: "570px" },
+                "max-400": { max: "400px" },
+            },
             backgroundImage: {
                 "background-pattern": "url('/imgs/background.svg')",
             },
