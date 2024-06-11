819a102ad34a5de950fad2d50a1f03babb4d06e0
Yoon <kohyc97@gmail.com>
2024-06-11 11:45:58 +0900

    feat: 프로필 정보 초기화 effect 추가

--- front/src/components/Profile.jsx
+++ front/src/components/Profile.jsx
@@ -3,6 +3,30 @@ import React from "react";
 const Profile = () => {
     const [asset, setAsset] = useState([]);
     const [userInfo, setUserInfo] = useState([]);
+    const userInfoAPI = "api url";
+    const assetAPI = "api url";
+
+    useEffect(() => {
+        //userInfo 초기화
+        axios
+            .get(userInfoAPI) //말고 post 써야 할거 같긴하다
+            .then((res) => {
+                setUserInfo(res.data);
+            })
+            .catch((err) => {
+                console.error("초기 유저 정보를 불러오는데 실패했습니다.", err);
+            });
+
+        //asset 초기화
+        axios
+            .get(assetAPI)
+            .then((res) => {
+                setAsset(res.data);
+            })
+            .catch((err) => {
+                console.error("초기 유저 자산을 불러오는데 실패했습니다.", err);
+            });
+    }, []);
 
     return (
         <div style={{ display: "flex" }}>
@@ -16,4 +40,4 @@ const Profile = () => {
     );
 };
 
-export default Landing;
+export default Profile;
