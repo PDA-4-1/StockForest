d60a821e5f546473cb45b392aca53ab97d939c5d
Yoon <kohyc97@gmail.com>
2024-06-11 09:47:35 +0900

    feat: 가제 추가 및 로그인, 회원가입 버튼 추가

--- front/src/routes/landingPage/Landing.jsx
+++ front/src/routes/landingPage/Landing.jsx
@@ -4,8 +4,12 @@ import Landing3D from "../../components/Landing3D";
 const Landing = () => {
     return (
         <div className="main-page-wrapper">
+            <h1>모여봐요 주식의 숲</h1>
+            <div style={{display:"flex"}}>
+            <button>로그인</button>
+            <button>회원가입</button>
+            </div>
             <Landing3D />            
-            <div>Login</div>
         </div>
     );
 };
