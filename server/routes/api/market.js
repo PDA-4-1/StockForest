253d1371247f7d9497760c0af1b99510fb4eda14
LimSeHyeon <dlatpgus1205@naver.com>
2024-06-12 21:28:57 +0900

    feat: api 라우팅 기본 세팅 작성

--- server/routes/api/market.js
+++ server/routes/api/market.js
@@ -1,8 +1,41 @@
-var express = require('express');
-var pool = require('../../config/db.connect.js');
+var express = require("express");
+var pool = require("../../config/db.connect.js");
 var router = express.Router();
 
+router.get("/:turn", async (req, res) => {
+    //TODO : 시장 화면 첫 진입 시 종목 목록 띄우기 위한 데이터 요청
+    // 종목명, 설명, 산업 + 등락률
+});
 
+router.get("/stock/:turn", async (req, res) => {
+    //TODO : 종목 주가 데이터 요청. 차트에 표시할 과거 1주일간의 데이터 요청
+});
 
+router.get("/:id", async (req, res) => {
+    //TODO : 랭킹 불러오기. TOP 10 + 본인 몇 등인지
+});
+
+router.post("/buy", async (req, res) => {
+    //TODO : 주식 매수. request body 값들 활용해서 DB 값 수정하기.
+    /*
+    request body : {
+	  stock_id,
+	  price,
+	  amount
+    }
+    */
+});
+
+router.get("/sell/:id/:stodkid/:turn", async (req, res) => {
+    //TODO : 현재 가지고 있는 주식 수, 가격 불러오기
+});
+
+router.post('/sell', async (req, res) => {
+    //TODO : 몇 개 팔건지 요청받아서 뺄거 빼고 필요한 계산 수행
+});
+
+router.post('/turn/:id', async (req, res) => {
+    //TODO : 턴 넘기기. 다음 주 날짜로 바꾸기, 뉴스 정보 있으면 받아오기
+})
 
 module.exports = router;
--- server/routes/api/quiz.js
+++ server/routes/api/quiz.js
@@ -2,7 +2,13 @@ var express = require('express');
 var pool = require('../../config/db.connect.js');
 var router = express.Router();
 
+router.post('/response', async (req, res) => {
+    //TODO : 사용자가 퀴즈에 답한 내용을 기록. 어떤 종목인지, 오를지 내릴지
+})
 
+router.post('/answer', async(req, res) => {
+    //TODO : 퀴즈 답 확인, 맞았을 경우 보상받은 포인트까지 계산할 것
+})
 
 
 module.exports = router;
