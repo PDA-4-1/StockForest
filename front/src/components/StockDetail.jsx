ac6c53ff342993598d63ae34de4149d6a0a2be91
hyuna <kook980316@gmail.com>
2024-06-13 08:54:08 +0900

    feat: 주식 디테일 레이아웃 설정 후 버튼 삽입

--- front/src/components/StockDetail.jsx
+++ front/src/components/StockDetail.jsx
@@ -1,7 +1,22 @@
+import StockButton from "./StockButton";
+
 export default function StockDetail() {
     return (
-        <div className="bg-white w-full h-full col-span-3">
-            <p>여기 주식 이름</p>
+        <div className="bg-white w-full h-full col-span-3 px-6 py-9 flex flex-col justify-between">
+            <div className="flex justify-between items-center">
+                <span>여기 주식 이름</span>
+                <span>가격</span>
+                <span>여기 상승하락</span>
+                <div className="flex space-x-6">
+                    <StockButton purpo="buy" onClick={() => {}} />
+                    <StockButton purpo="sell" onClick={() => {}} />
+                </div>
+            </div>
+            <div className="flex">
+                <div className="w-28 h-28 bg-black rounded-full"></div>
+                <p>여기 회사 설명할거임</p>
+            </div>
+            <div className="h-[200px] bg-black text-white">여기 차트 자리</div>
             <div></div>
         </div>
     );
