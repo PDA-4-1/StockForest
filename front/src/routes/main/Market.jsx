c7b8307c3dbc06befe63e166a0c9b5db69df0d6d
hyuna <kook980316@gmail.com>
2024-06-12 10:17:38 +0900

    design: 시장 레이아웃 생성

--- front/src/routes/main/Market.jsx
+++ front/src/routes/main/Market.jsx
@@ -3,7 +3,14 @@ import Navbar from "../../components/Navbar";
 const Market = () => {
     return (
         <div className="bg-background-pattern bg-cover bg-center h-screen">
-          <Navbar/>
+            <Navbar />
+            <div className="grid grid-cols-3 w-full h-[calc(100%_-_69.6px)] gap-6 p-6">
+                <div className="bg-back-yellow col-span-2 h-full"></div>
+                <div className="grid grid-rows-3 h-full gap-6">
+                    <div className="bg-back-yellow">여기 프로필 부분</div>
+                    <div className="row-span-2 bg-back-yellow">여기는 랭킹 부분</div>
+                </div>
+            </div>
         </div>
     );
 };
