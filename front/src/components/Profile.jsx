cbf540311715fd81853bfd13562374ca3add7291
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-13 14:56:59 +0900

    feat: 농장 테스트

--- front/src/components/Profile.jsx
+++ front/src/components/Profile.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Profile = () => {
     const [asset, setAsset] = useState([]);
@@ -29,16 +31,28 @@ const Profile = () => {
     }, []);
 
     return (
-        <div style={{ display: "flex" }}>
-            <img>{userInfo.image}</img>
-            <div>
-                <p>{userInfo.nickname}</p>
-                <p>수익률: {asset.user_returns}%</p>
+        <div className="w-[400px] h-[300px] scale-200 bg-[url('/imgs/loginform.svg')] bg-no-repeat bg-center bg-cover flex flex-col justify-center items-center">
+            <div className="flex justify-center items-center">
+                <div className="w-[50px] h-[50px] rounded-full bg-black mr-4">
+                    <img>{userInfo.image}</img>
+                </div>
+                <div className="text-left">
+                    <div> 테스트닉네임{userInfo.nickname}</div>
+                    <div className="mt-[10px]">
+                        수익률: ??.??{asset.user_returns}%
+                    </div>
+                </div>
             </div>
-            <p>자산: {asset.user_pdi} 프디</p>
-            <div>
-                <p>{userInfo.turns} 턴</p>
-                <button>다음 턴</button>
+
+            <div className="mt-[30px]">
+                <div>자산: 100{asset.user_pdi} 프디</div>
+            </div>
+
+            <div className="flex items-center mt-[30px] justify-center gap-3">
+                <div>10{userInfo.turns}턴</div>
+                <button className="bg-yellow-500 text-white px-4 py-2 rounded">
+                    다음 턴
+                </button>
             </div>
         </div>
     );
--- front/src/routes/main/Market.jsx
+++ front/src/routes/main/Market.jsx
@@ -1,9 +1,15 @@
+import Field from "../../components/Field";
 import Navbar from "../../components/Navbar";
+import Profile from "../../components/Profile";
 
 const Market = () => {
     return (
         <div className="bg-background-pattern bg-cover bg-center h-screen">
-          <Navbar/>
+            <Navbar />
+            <div className="flex justify-end mt-[50px]">
+                <Profile/>
+            </div>
+            <Field/>
         </div>
     );
 };
