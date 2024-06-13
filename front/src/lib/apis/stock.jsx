785205d269a2c76151bb26684158245092fd813f
hyuna <kook980316@gmail.com>
2024-06-14 08:56:23 +0900

    feat: stock 리스트 불러오는 API 추가

---
+++ front/src/lib/apis/stock.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = "/api/market";
+const service = axios.create({ withCredentials: true, baseURL: BASE_URL });
+
+export async function GetStockList(turn) {
+    const res = await service.get(`/${turn}`);
+    return res.data;
+}
