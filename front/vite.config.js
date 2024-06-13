4b61ddf081a402262c6373069c3f9249737b8c12
hyuna <kook980316@gmail.com>
2024-06-13 13:49:35 +0900

    chore: 경로 추가

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            // 절대경로로 접근하기
+            { find: "~/components", replacement: "/src/components" },
+            { find: "~/lib", replacement: "/src/lib" },
+            { find: "~/routers", replacement: "/src/routers" },
+            { find: "~/routes", replacement: "/src/routes" },
+            { find: "~/store", replacement: "/src/store" },
+            { find: "~/public", replacement: "/public" },
+        ],
+    },
+});
