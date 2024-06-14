3516aaf963df1e111bace4447fe397c5c063646c
hyuna <kook980316@gmail.com>
2024-06-14 11:41:14 +0900

    feat: redux 초기 세팅

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,37 +2,29 @@ import React, { useState } from "react";
 import { RouterProvider, Route, Routes, BrowserRouter } from "react-router-dom";
 import { routerObj } from "./routers/router";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function renderRoutes(routesObj) {
     return routesObj.map((route) => {
         if (route.children) {
             return (
-                <Route
-                    key={route.path}
-                    path={route.path}
-                    index={route.index}
-                    element={route.element}
-                >
+                <Route key={route.path} path={route.path} index={route.index} element={route.element}>
                     {route.children ? renderRoutes(route.children) : null}
                 </Route>
             );
         }
-        return (
-            <Route
-                key={route.path}
-                path={route.path}
-                index={route.index}
-                element={route.element}
-            />
-        );
+        return <Route key={route.path} path={route.path} index={route.index} element={route.element} />;
     });
 }
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>{renderRoutes(routerObj)}</Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>{renderRoutes(routerObj)}</Routes>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
