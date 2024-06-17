eb0deac86607ba6dff87f1b1cca9b387ada5dab7
Yoon <kohyc97@gmail.com>
2024-06-18 08:27:29 +0900

    feat: 배경화면 추가

--- front/src/routes/landingPage/Landing.jsx
+++ front/src/routes/landingPage/Landing.jsx
@@ -3,17 +3,7 @@ import Landing3D from "../../components/Landing3D";
 
 const Landing = () => {
     return (
-        <div
-            className="main-page-wrapper"
-            style={{
-                position: "relative",
-                height: "100vh",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
+        <div className="bg-background-pattern bg-cover bg-center h-screen">
             <h1>모여봐요 주식의 숲</h1>
             <div style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
                 <button>로그인</button>
