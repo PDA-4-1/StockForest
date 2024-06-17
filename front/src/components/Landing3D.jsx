ca43a083514d4d54362b8302b40db01ea8d0ebed
Yoon <kohyc97@gmail.com>
2024-06-18 08:37:14 +0900

    style: canvas 크기 변경

--- front/src/components/Landing3D.jsx
+++ front/src/components/Landing3D.jsx
@@ -161,7 +161,7 @@ const ThreeModelViewer = () => {
             ref={containerRef}
             style={{
                 width: "100%",
-                height: "400px",
+                height: "600px",
                 position: "absolute",
                 bottom: 0,
                 left: "50%",
