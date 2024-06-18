f012d05c67f9604ff215b62fdd54c7f032054750
LimSeHyeon <dlatpgus1205@naver.com>
2024-06-18 15:45:41 +0900

    feat: 사용자의 퀴즈 답 선택 내용 state에 저장 및 css 추가

--- front/src/routes/main/Quiz.jsx
+++ front/src/routes/main/Quiz.jsx
@@ -1,14 +1,41 @@
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
+import moment from "moment-timezone";
 import { FaChevronDown } from "react-icons/fa";
 import { FaChevronUp } from "react-icons/fa";
+import { TbTriangleFilled } from "react-icons/tb";
+import { TbTriangleInvertedFilled } from "react-icons/tb";
+import { UpdateQuiz } from "~/lib/apis/quiz";
 
 const Quiz = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [stockName, setStockName] = useState("주식 이름");
+    const [stockId, setStockId] = useState(0);
+    const [date, setDate] = useState("2024-06-18");
+    const [upDown, setUpDown] = useState(true); //상승이면 true
+
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
 
-    
+    //date 설정 -> 오늘 날짜로 설정하기(서울 기준)
+    useEffect(() => {
+        const currentDate = moment().tz("Asia/Seoul").format("YYYY-MM-DD");
+        setDate(currentDate);
+        // console.log(currentDate);
+    });
+
+    useEffect(() => {
+        console.log(upDown);
+    }, [upDown]);
+
+    const changeToUp = () => {
+        setUpDown(true);
+    };
+
+    const changeToDown = () => {
+        setUpDown(false);
+    };
 
     // open API를 이용해서 퀴즈에 쓸 투자전략 불러오기 -> 앞으로 1주일 간 꾸준히 불러보고 판단
     /* 사용자 응답 받아서 post 요청하기
@@ -19,18 +46,19 @@ const Quiz = () => {
         - axios로 api 요청 전송
         - 응답 확인하기
     */
+    // 어제자 답 확인 클릭 시 -> 한국투자증권 api 요청해서 어제자, 오늘자 일봉 불러와서 비교하기
 
     return (
         <div className="bg-background-pattern bg-cover bg-center h-screen">
             <Navbar />
             <div className="flex flex-row">
-                <div className="basis-1/2">
+                <div className="basis-1/2 flex justify-center">
                     <button
                         type="button"
-                        className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="inline-flex items-center justify-between w-60 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         onClick={toggleDropdown}
                     >
-                        주식 이름
+                        {stockName}
                         {isOpen ? <FaChevronDown /> : <FaChevronUp />}
                     </button>
                 </div>
@@ -47,6 +75,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(1);
+                                    setStockName("콩순전자");
+                                    toggleDropdown();
+                                }}
                             >
                                 콩순전자
                             </a>
@@ -54,6 +87,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(2);
+                                    setStockName("초콜릿통신");
+                                    toggleDropdown();
+                                }}
                             >
                                 초콜릿통신
                             </a>
@@ -61,6 +99,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(3);
+                                    setStockName("뉴진수퍼노바");
+                                    toggleDropdown();
+                                }}
                             >
                                 뉴진수퍼노바
                             </a>
@@ -68,6 +111,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(4);
+                                    setStockName("주식회사붕붕");
+                                    toggleDropdown();
+                                }}
                             >
                                 주식회사붕붕
                             </a>
@@ -75,6 +123,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(5);
+                                    setStockName("미미네약국");
+                                    toggleDropdown();
+                                }}
                             >
                                 미미네약국
                             </a>
@@ -82,6 +135,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(6);
+                                    setStockName("철수건설");
+                                    toggleDropdown();
+                                }}
                             >
                                 철수건설
                             </a>
@@ -89,6 +147,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(7);
+                                    setStockName("올리브업");
+                                    toggleDropdown();
+                                }}
                             >
                                 올리브업
                             </a>
@@ -96,6 +159,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(8);
+                                    setStockName("숲속여관");
+                                    toggleDropdown();
+                                }}
                             >
                                 숲속여관
                             </a>
@@ -103,6 +171,11 @@ const Quiz = () => {
                                 href="#"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 role="menuitem"
+                                onClick={() => {
+                                    setStockId(9);
+                                    setStockName("과학나라");
+                                    toggleDropdown();
+                                }}
                             >
                                 과학나라
                             </a>
@@ -110,11 +183,30 @@ const Quiz = () => {
                     </div>
                 )}
                 <div className="flex basis-1/2 items-center">
-                    <div className="flex basis-1/2 justify-center">
-                        <div className="basis-1/2 text-center">상승버튼</div>
-                        <div className="basis-1/2 text-center">하락버튼</div>
+                    <div className="flex basis-1/2 justify-between">
+                        <button
+                            className="basis-1/2 text-center flex items-center justify-evenly"
+                            onClick={changeToUp}
+                        >
+                            <TbTriangleFilled />
+                            상승버튼
+                        </button>
+                        <button
+                            className="basis-1/2 text-center flex items-center justify-evenly"
+                            onClick={changeToDown}
+                        >
+                            <TbTriangleInvertedFilled />
+                            하락버튼
+                        </button>
                     </div>
-                    <div className="basis-1/2 text-center">저장</div>
+                    <div className="basis-1/2 flex justify-center">
+                        <button
+                            className="w-40 text-center"
+                            onClick={postAnswer}
+                        >
+                            저장
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
