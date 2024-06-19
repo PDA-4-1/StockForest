fe42ce5ddf73e8f64617e7dd95eaa73f32ae48af
Yoon <93638922+Yoon-97@users.noreply.github.com>
2024-06-19 13:38:17 +0900

    chor: vite 기본 포트 변경

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from "vite";
+                                                                                                                              9,5           Allimport { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
-        proxy: {
-            "/api": {
-                target: "http://127.0.0.1:4000",
-                // changeOrigin: true,
-            },
-        },
+        port: 3000,
     },
     resolve: {
         alias: [
