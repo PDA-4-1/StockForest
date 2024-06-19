2f5b2ced6d1a075e42bda97c506f50e18309e51f
hyuna <kook980316@gmail.com>
2024-06-19 15:51:46 +0900

    feat: redux에 유저 정보 저장

---
+++ front/src/store/userSlice.jsx
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: {
+        nickname: "",
+        returns: "",
+        user_pdi: 0,
+        turn: 1,
+        img: 1,
+    },
+};
+
+const userSlice = createSlice({
+    name: "userSlice",
+    initialState,
+    reducers: {
+        reset: () => {
+            return initialState;
+        },
+        saveUser: (state, action) => {
+            const newUser = action.payload;
+            // console.log(newUser);
+            state.user = newUser;
+        },
+        savePdi: (state, action) => {
+            const newPdi = state.user.user_pdi + action.payload;
+            state.user.user_pdi = newPdi;
+        },
+    },
+});
+
+export default userSlice.reducer;
+export const { reset, saveUser, savePdi } = userSlice.actions;
