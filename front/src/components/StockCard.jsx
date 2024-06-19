00e529676719cbcd2d1bcd792f6dea6693d5925f
hyuna <kook980316@gmail.com>
2024-06-20 08:31:32 +0900

    update: 주가 변동이 없을 때는 화살표 보이지 않음

--- front/src/components/StockCard.jsx
+++ front/src/components/StockCard.jsx
@@ -9,20 +9,21 @@ export default function StockCard(props) {
         <div
             className={`${
                 selected == name ? "bg-select-green" : "bg-white"
-            } w-full rounded-3xl flex items-center space-x-2 justify-center px-3 space-x-2 hover:cursor-pointer hover:bg-select-green max-w-[180px]`}
+            } w-full rounded-3xl flex items-center justify-center px-3 space-x-2 hover:cursor-pointer hover:bg-select-green max-w-[180px]`}
             onClick={onClick}
         >
             <div className="w-9 h-9 rounded-full bg-black"></div>
             <div className="text-sm min-w-[100px]">
                 <p className={selected == name ? "text-white" : ""}>{name}</p>
                 <div
-                    className={
-                        diff >= 0 ? "text-shinhan-red flex justify-between" : "text-shinhan-blue flex justify-between"
-                    }
+                    className={`
+                        ${
+                            diff > 0 ? "text-shinhan-red" : diff < 0 ? "text-shinhan-blue" : "text-black"
+                        }  flex justify-between`}
                 >
                     <span>{price.toLocaleString()}</span>
                     <div className="flex items-center ml-2">
-                        <IoTriangleSharp className={diff <= 0 && "rotate-180"} />
+                        <IoTriangleSharp className={diff < 0 ? "rotate-180" : diff == 0 ? "hidden" : ""} />
                         <span className="min-w-[34px] text-right">{Math.abs(diff).toLocaleString()}</span>
                     </div>
                 </div>
