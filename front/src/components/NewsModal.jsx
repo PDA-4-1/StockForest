d699fb02e28eb0fa6e77fc0a2cee30491e94557b
hyuna <kook980316@gmail.com>
2024-06-21 14:36:13 +0900

    design: 뉴스 모달 떴을 때 0.5초간 회전 효과

--- front/src/components/NewsModal.jsx
+++ front/src/components/NewsModal.jsx
@@ -1,18 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function NewsModal(props) {
     const onHide = props.onHide;
     const newsList = props.newsList;
+    const [isSpin, setIsSpin] = useState(true);
     const handleModalClick = (e) => {
         if (e.target === e.currentTarget) {
             onHide();
         }
     };
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsSpin(false);
+        }, 500);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div
             className="z-10 bg-black/30 w-screen h-screen fixed left-0 top-0 flex justify-center items-center"
             onClick={handleModalClick}
         >
-            <div className="w-fit h-3/4 bg-[#FCFCFC] p-12 flex flex-col items-center">
+            <div
+                className={`w-fit h-3/4 bg-[#FCFCFC] p-12 flex flex-col items-center ${
+                    isSpin ? "animate-spin-fast" : ""
+                }`}
+            >
                 <p className="text-7xl text-[#3E424B] w-fit border-t-4 border-[#4F4E53] pt-6">
                     THE DALIY <span className="text-[#EA071B]">NEWS</span>
                 </p>
