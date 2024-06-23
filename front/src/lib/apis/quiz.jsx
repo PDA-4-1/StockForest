29dfa9e47d0e553b4d4e21ba0bb0ffb42d5d265b
LimSeHyeon <dlatpgus1205@naver.com>
2024-06-24 08:27:12 +0900

    feat: 서버에서 처리하기로 한 로직 삭제

--- front/src/lib/apis/quiz.jsx
+++ front/src/lib/apis/quiz.jsx
@@ -34,38 +34,3 @@ export async function AnswerQuiz() {
     const res = await service.patch("/answer");
     return res.data;
 }
-
-// 일봉 데이터 요청
-export async function dailyStock(date) {
-    const token = await shinhanService.post("/oauth2/tokenP", {
-        headers: {
-            grant_type: "client_credentials",
-            appkey: import.meta.env.VITE_appKey,
-            appsecret: import.meta.env.VITE_appSecret,
-        },
-    });
-    console.log(token);
-    const accToken = token.access_token;
-    const endDate = moment(date).format("YYYYMMDD");
-    const startDate = moment(date).subtract(1, "days").format("YYYYMMDD");
-    const res = await shinhanService.get(
-        "/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice",
-        {   
-            headers: {
-                authorization: accToken,
-                appkey: import.meta.env.VITE_appKey,
-                appsecret: import.meta.env.VITE_appSecret,
-                tr_id: import.meta.env.VITE_tr_id,
-            },
-            body: {
-                fid_cond_mrkt_div_code: "J", //주식
-                fid_input_iscd: "005930", //종목코드
-                fid_input_date_1: startDate, //시작일자
-                fid_input_date_2: endDate, //종료일자
-                fid_period_div_code: "D", //일봉
-                fid_org_adj_prc: "0", //수정주가
-            },
-        }
-    );
-    // console.log(res);
-}
