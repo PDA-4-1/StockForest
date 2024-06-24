25a2a6eee4b75b9d0e77523c4dea7a8d641e04a9
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-24 18:23:07 +0900

    design: 회원가입 디자인 수정 및 alert 디자인 변경

--- front/src/routes/signup/Signup.jsx
+++ front/src/routes/signup/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { Toast } from "../../components/Toast";
 
 const Signup = () => {
     const {
@@ -20,71 +21,78 @@ const Signup = () => {
             });
 
             if (response.status === 200) {
-                alert("회원가입 성공");
+                Toast.fire("회원가입 성공!", "", "success");
                 navigate("/login");
             }
         } catch (error) {
             if (error.response) {
                 alert(error.response.data.message);
             } else {
-                alert("회원가입 중 오류가 발생했습니다.");
+                Toast.fire("회원가입 중 오류가 발생했습니다!", "", "error");
             }
         }
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="relative bg-background-pattern bg-cover bg-center h-screen">
-                <div className="flex justify-center pt-5 scale-75">
+            <div className="relative bg-background-pattern bg-cover bg-center h-screen grid grid-rows-6">
+                <div className="flex justify-center pt-5 scale-75 row-span-2">
                     <img
                         src="/imgs/loginlogo.svg"
                         alt="로그인로고"
                         className="max-400:w-[300px]"
                     />
                 </div>
-                <div className="flex justify-center scale-90 -mt-10 bg-[url('/imgs/loginform.svg')] bg-no-repeat bg-center bg-contain lg:h-[600px] max-lg:h-[500px]">
-                    <div className="flex flex-col items-center justify-center p-4">
-                        <h1 className="text-[40px] text-[#505050] mb-10 max-570:text-[30px]">
-                            회원가입
-                        </h1>
-                        <input
-                            type="text"
-                            placeholder="닉네임"
-                            className="mb-2 p-2 border border-gray-300 rounded w-full max-w-md md:max-w-xs max-570:w-[150px]"
-                            {...register("nickname", {
-                                required: "닉네임을 입력해주세요.",
-                                maxLength: {
-                                    value: 20,
-                                    message: "닉네임은 20자 이내여야 합니다.",
-                                },
-                            })}
-                        />
-                        {errors.nickname && (
-                            <p className="text-red-500">
-                                {errors.nickname.message}
-                            </p>
-                        )}
-                        <input
-                            type="password"
-                            placeholder="비밀번호"
-                            className="mt-2 mb-2 p-2 border border-gray-300 rounded w-full max-w-md md:max-w-xs max-570:w-[150px]"
-                            {...register("password", {
-                                required: "비밀번호를 입력해주세요.",
-                                minLength: {
-                                    value: 3,
-                                    message:
-                                        "비밀번호는 최소 3자 이상이어야 합니다.",
-                                },
-                            })}
-                        />
-                        {errors.password && (
-                            <p className="text-red-500">
-                                {errors.password.message}
-                            </p>
-                        )}
+                <div className="flex justify-center bg-[url('/imgs/loginform.svg')] bg-no-repeat bg-center bg-contain row-span-3">
+                    <div className="flex flex-col items-center justify-center p-4 w-full">
+                        <div className="grid grid-rows-2">
+                            <div className="flex items-center justify-center row-span-1">
+                                <h1 className="text-[3vw] text-[#505050] mb-10">
+                                    회원가입
+                                </h1>
+                            </div>
+                            <div className="flex flex-col items-center justify-center row-span-1">
+                                <input
+                                    type="text"
+                                    placeholder="닉네임"
+                                    className="p-2 border border-gray-300 rounded w-full "
+                                    {...register("nickname", {
+                                        required: "닉네임을 입력해주세요.",
+                                        maxLength: {
+                                            value: 20,
+                                            message:
+                                                "닉네임은 20자 이내여야 합니다.",
+                                        },
+                                    })}
+                                />
+                                {errors.nickname && (
+                                    <p className="text-red-500">
+                                        {errors.nickname.message}
+                                    </p>
+                                )}
+                                <input
+                                    type="password"
+                                    placeholder="비밀번호"
+                                    className="p-2 border border-gray-300 rounded w-full mt-2"
+                                    {...register("password", {
+                                        required: "비밀번호를 입력해주세요.",
+                                        minLength: {
+                                            value: 3,
+                                            message:
+                                                "비밀번호는 최소 3자 이상이어야 합니다.",
+                                        },
+                                    })}
+                                />
+                                {errors.password && (
+                                    <p className="text-red-500">
+                                        {errors.password.message}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
                         <button
                             type="submit"
-                            className="mt-6 p-2 bg-yellow-400 rounded w-full max-w-md md:max-w-xs"
+                            className="mt-6 p-2 bg-yellow-400 rounded w-[200px]"
                         >
                             회원가입
                         </button>
