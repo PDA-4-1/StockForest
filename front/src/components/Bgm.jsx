f5756cd9b7e07b46e49e7355bd4c271ff76408e9
Yoon <kohyc97@gmail.com>
2024-06-24 16:32:18 +0900

    feat: iframe -> audio tag 로 변경

--- front/src/components/Bgm.jsx
+++ front/src/components/Bgm.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useRef, useEffect } from "react";
 
 const Bgm = () => {
     const bgmStatus = useSelector((state) => state.stock.bgmOn);
     const bgmUrl = "/bgm/bgm_1h.mp3";
+    const audioRef = useRef(null);
+
+    useEffect(() => {
+        if (audioRef.current) {
+            if (bgmStatus) {
+                audioRef.current.play();
+            } else {
+                audioRef.current.pause();
+            }
+        }
+    }, [bgmStatus]);
 
     console.log(`음악 실행 여부: ${bgmStatus}`);
     return (
-        <>
-            <iframe
-                src={bgmUrl}
-                allow="autoplay"
-                style={{ display: "none" }}
-            ></iframe>
-        </>
+        <audio ref={audioRef} src={bgmUrl} loop style={{ display: "none" }} />
     );
 };
 
