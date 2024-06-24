b46255f556a6a7305d2aeb20b7204d93c7fb444e
Yoon <kohyc97@gmail.com>
2024-06-24 15:39:25 +0900

    feat: bgm state를 redux state로 변경

--- front/src/components/Bgm.jsx
+++ front/src/components/Bgm.jsx
@@ -1,30 +1,19 @@
 import React from "react";
-import { useState, useEffect } from "react";
-
+import { useSelector } from "react-redux";
 
 const Bgm = () => {
-  const audioRef = React.useRef()
-  const [isPlay, setIsPlay] = useState(true);
-  const bgmUrl = "/bgm/bgm_1h.mp3";
-  // useEffect(()=>{
-  //   console.log(audioRef)
-  // audioRef.current.play()
-  // }, [])
-  console.log(`음악 실행 여부: ${isPlay}`);
+    const bgmStatus = useSelector((state) => state.user.bgmOn);
+    const bgmUrl = "/bgm/bgm_1h.mp3";
+
+    console.log(`음악 실행 여부: ${bgmStatus}`);
     return (
-      //isPlay redux 처리
-      <>
-        {/* {isPlay && (
-          <audio
-            src= {bgmUrl}
-            ref={audioRef}
-            // autoPlay={isPlay}
-            volume={1.0}
-            > 
-          </audio>
-        )} */}
-        <iframe src= {bgmUrl} allow="autoplay" style={{ display: "none" }}></iframe>
-      </>
+        <>
+            <iframe
+                src={bgmUrl}
+                allow="autoplay"
+                style={{ display: "none" }}
+            ></iframe>
+        </>
     );
 };
 
