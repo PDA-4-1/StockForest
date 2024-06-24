bb6391e8c611e8fbea3129b439413f38e6df4389
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-24 23:36:47 +0900

    design: 폰트 추가

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,15 +1,31 @@
 module.exports = {
+    mode: "jit",
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
             screens: {
-                "max-md": { max: "768px" },
-                "max-lg": { max: "1024px" },
-                "max-570": { max: "570px" },
-                "max-400": { max: "400px" },
+                "3xl": "1820px",
+                // "2xl": "1500px",
             },
             backgroundImage: {
                 "background-pattern": "url('/imgs/background.svg')",
+            },
+            colors: {
+                "back-yellow": "#FEED9F",
+                "shinhan-red": "#E11A1A",
+                "shinhan-blue": "#2173CF",
+                "select-green": "#88C9A1",
+                "back-red": "#FDE6EA",
+                "back-blue": "#F2F5FC",
+                "modal-yellow": "#FDF9EA",
+                "back-grey": "#EFF0F6",
+            },
+            animation: {
+                "spin-fast": "spin 0.3s linear infinite",
+            },
+            fontFamily: {
+                mango: ["MangoByeolbyeol", "sans-serif"],
+                jamsil: ["TheJamsil5Bold", "sans-serif"],
             },
         },
     },
