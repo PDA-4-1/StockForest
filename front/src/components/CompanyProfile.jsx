4cf1a886f9b284ea66d2b2ce8028c2466ad04535
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-24 23:36:17 +0900

    design: 프로필 요소 변경

--- front/src/components/CompanyProfile.jsx
+++ front/src/components/CompanyProfile.jsx
@@ -1,33 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const CompanyProfile = ({ visible, onClose, image }) => {
+const CompanyProfile = ({
+    visible,
+    onClose,
+    image,
+    stock,
+    name,
+    currentPrice,
+}) => {
+    const navigate = useNavigate();
+    const userInfo = useSelector((state) => state.user.user);
+
+    const goMarket = () => {
+        navigate("/market");
+    };
+
     return (
         <div
-            className={`fixed bottom-0 right-0 bg-[url('/imgs/companyprofile.svg')] bg-no-repeat bg-center bg-contain h-[500px] w-[350px] transition-transform duration-300 ${
+            className={`bg-[url('/imgs/companyprofile.svg')] bg-no-repeat bg-center bg-contain w-full h-full scale-95 transition-transform duration-300 ${
                 visible ? "translate-x-0" : "translate-x-full"
             }`}
         >
-            {image && (
-                <img
-                    src={image}
-                    className="w-[100px] h-[100px] absolute left-[45px] top-[40px]"
-                    alt="Company"
-                />
-            )}
-            <div className="absolute left-[170px] top-[80px]">삼성전자</div>
-            <div className="absolute grid grid-cols-1 gap-4 mb-4 top-[200px] left-[70px]">
-                <div>나의 주식수</div>
-                <div>평단가</div>
-                <div>수익률</div>
+            <div className="relative top-[5vh] left-[3vw] flex items-center">
+                {image && (
+                    <div className="w-[10vw] h-[10vh]">
+                        <img
+                            src={image}
+                            className="w-full h-full object-contain"
+                            alt="Company"
+                        />
+                    </div>
+                )}
+                <div>{name}</div>
             </div>
-            <button className="absolute top-[400px] left-[140px]">
-                팔러가기
-            </button>
+            <div className="relative grid grid-cols-1 gap-4 mb-4 top-[10vh] left-[7vw]">
+                <div>나의 주식수 : {stock.quantity}개</div>
+                <div>평단가 : {stock.avg_price}원</div>
+                <div>현재가 : {currentPrice}</div>
+                <div>수익률 : {stock.returns}%</div>
+            </div>
             <button
-                onClick={onClose}
-                className="absolute top-[50px] right-[60px]"
+                onClick={goMarket}
+                className="relative top-[15vh] left-[10vw] bg-yellow-500 text-white px-4 py-2 rounded hover:brightness-75"
             >
-                X
+                팔러가기
             </button>
         </div>
     );
