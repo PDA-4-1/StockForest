16ab900d5e9f66a7b4b611077c2e136d13361e83
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-25 16:54:29 +0900

    feat: 수익률 소수점 두자리까지 출력

--- front/src/components/CompanyProfile.jsx
+++ front/src/components/CompanyProfile.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 const CompanyProfile = ({
     visible,
@@ -12,7 +11,6 @@ const CompanyProfile = ({
     currentPrice,
 }) => {
     const navigate = useNavigate();
-    const userInfo = useSelector((state) => state.user.user);
 
     const goMarket = () => {
         navigate("/market");
@@ -37,12 +35,23 @@ const CompanyProfile = ({
                             </div>
                         )}
                         <div>{name}</div>
+                        <button
+                            onClick={onClose}
+                            className="bg-wood-opacity-50 text-white px-2 py-2 rounded hover:brightness-75"
+                        ></button>
                     </div>
                     <div className="grid grid-cols-1 gap-2 mb-4 row-span-2">
                         <div>나의 주식수 : {stock.quantity}개</div>
                         <div>평단가 : {stock.avg_price}원</div>
                         <div>현재가 : {currentPrice}</div>
-                        <div>수익률 : {stock.returns}%</div>
+                        <div>
+                            수익률 :{" "}
+                            {stock.returns !== undefined &&
+                            stock.returns !== null
+                                ? stock.returns.toFixed(2)
+                                : "데이터 없음"}
+                            %
+                        </div>
                     </div>
                     <button
                         onClick={goMarket}
