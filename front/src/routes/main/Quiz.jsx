595edf8cb5418af42eaceb8aed66c741a117cfaf
LimSeHyeon <dlatpgus1205@naver.com>
2024-06-25 17:13:59 +0900

    update: 주가 예측 개별 버튼 -> 드롭다운 버튼으로 변경

--- front/src/routes/main/Quiz.jsx
+++ front/src/routes/main/Quiz.jsx
@@ -11,6 +11,7 @@ import QuizAnswerModal from "../../components/QuizAnswerModal";
 
 const Quiz = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isUpDownOpen, setIsUpDownOpen] = useState(false);
     const [stockName, setStockName] = useState("주식 이름");
     const [stockId, setStockId] = useState(0);
     const [date, setDate] = useState("2024-06-18");
@@ -31,6 +32,10 @@ const Quiz = () => {
         setIsOpen(!isOpen);
     };
 
+    const upDownToggleDropdown = () => {
+        setIsUpDownOpen(!isUpDownOpen);
+    };
+
     //date 설정 -> 오늘 날짜로 설정하기(서울 기준)
     useEffect(() => {
         const currentDate = moment().tz("Asia/Seoul").format("YYYY-MM-DD");
@@ -43,13 +48,13 @@ const Quiz = () => {
         getContent();
     }, []);
 
-    const changeToUp = () => {
-        setUpDown(true);
-    };
+    // const changeToUp = () => {
+    //     setUpDown(true);
+    // };
 
-    const changeToDown = () => {
-        setUpDown(false);
-    };
+    // const changeToDown = () => {
+    //     setUpDown(false);
+    // };
 
     const postAnswer = async () => {
         const result = await UpdateQuiz(stockId, date, upDown);
@@ -96,7 +101,7 @@ const Quiz = () => {
             </div>
             <div className="flex flex-row">
                 <div className="basis-1/3 flex justify-end relative">
-                    <div className="relative">
+                    <div className="relative px-1">
                         <button
                             type="button"
                             className="inline-flex items-center justify-between w-60 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -228,21 +233,56 @@ const Quiz = () => {
                     </div>
                 </div>
                 <div className="flex basis-1/3 items-center">
-                    <div className="flex basis-1/2 justify-between">
+                    <div className="relative flex basis-1/2 justify-between px-1">
                         <button
-                            className="basis-1/2 text-center flex items-center justify-evenly"
-                            onClick={changeToUp}
+                            type="button"
+                            className="inline-flex items-center justify-between w-60 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            onClick={upDownToggleDropdown}
                         >
-                            <TbTriangleFilled />
-                            상승버튼
+                            {upDown ? "상승":"하락"}
+                            {isUpDownOpen ? <FaChevronDown /> : <FaChevronUp />}
                         </button>
-                        <button
-                            className="basis-1/2 text-center flex items-center justify-evenly"
-                            onClick={changeToDown}
-                        >
-                            <TbTriangleInvertedFilled />
-                            하락버튼
-                        </button>
+
+                        {isUpDownOpen && (
+                            <div className="origin-top-right absolute right-0 top-full mt-0 w-60 max-h-60 overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                <div
+                                    className="py-1"
+                                    role="menu"
+                                    aria-orientation="vertical"
+                                    aria-labelledby="options-menu"
+                                >
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem"
+                                        onClick={() => {
+                                            setUpDown(1);
+                                            upDownToggleDropdown();
+                                        }}
+                                    >
+                                        <div className="flex text-center items-center justify-evenly">
+                                            <TbTriangleFilled />
+                                            상승
+                                        </div>
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem"
+                                        onClick={() => {
+                                            setUpDown(0);
+                                            upDownToggleDropdown();
+                                        }}
+                                    >
+                                        <div className="flex text-center items-center justify-evenly">
+                                            <TbTriangleInvertedFilled />
+                                            하락
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        )}
+
                     </div>
                     <div className="basis-1/2 flex justify-center">
                         <button
