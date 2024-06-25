be0133cb9454c28dd6d3339de5b3bca0bf0d798f
Yoon <kohyc97@gmail.com>
2024-06-25 10:55:27 +0900

    feat: user table 초기화

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,7 +3,6 @@ var pool = require("../../config/db.connect.js");
 var router = express.Router();
 const bcrypt = require("bcrypt");
 const { createToken, verifyToken } = require("../../utils/auth");
-const INITIAL_ASSET = 100000;
 
 function getRandomInt1to4() {
     return Math.floor(Math.random() * 4) + 1;
@@ -137,8 +136,28 @@ router.post("/signin", async (req, res) => {
     }
 });
 
-router.get("/exist/:nick", async (req, res) => {
-    //TODO : 닉네임 중복 확인 로직 구현
+router.patch("/ending", async (req, res) => {
+    //TODO: 모든 턴 종료시 사용자 정보 비우기
+    try {
+        //user table -> turn 초기화
+        const query = `Update user set turn=0 where id=?;`;
+        await pool.query(query, [userId]);
+
+        //hold_stock table -> 해당 유저 값 다 삭제
+        const del_hold_stock_query = `Delete from hold_stock where user_id=?;`;
+        await pool.query(del_hold_stock_query, [userId]);
+        const add_hold_stock_query = `Insert into hold_stock values (?,10,1,10000,0);`;
+        await pool.query(add_hold_stock_query, [userId]);
+
+        //ranking table -> 해당 유저 값 다 삭제
+        const set_ranking_query = `Update ranking set user_pdi=100000, user_returns=0 where user_id=?;`;
+        await pool.query(set_ranking_query, [userId]);
+
+        res.status(200).send("reset success");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
