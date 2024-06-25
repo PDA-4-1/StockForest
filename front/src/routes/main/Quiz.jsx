4ca73d8f6c41a37722abdf0eda84e40c67620653
LimSeHyeon <dlatpgus1205@naver.com>
2024-06-25 10:03:06 +0900

    feat: 드롭다운 선택창 높이 설정 및 스크롤 추가, 간격 제거

--- front/src/routes/main/Quiz.jsx
+++ front/src/routes/main/Quiz.jsx
@@ -86,7 +86,7 @@ const Quiz = () => {
                         </button>
 
                         {isOpen && (
-                            <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                            <div className="origin-top-right absolute right-0 top-full mt-0 w-60 max-h-60 overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                                 <div
                                     className="py-1"
                                     role="menu"
