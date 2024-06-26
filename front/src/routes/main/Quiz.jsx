92bc7b5a35f4603330f694ffa77049e5529362d2
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-26 13:15:16 +0900

    design: 퀴즈 페이지 디자인 수정

--- front/src/routes/main/Quiz.jsx
+++ front/src/routes/main/Quiz.jsx
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "~/components/Navbar";
 import moment from "moment-timezone";
-import { FaChevronDown } from "react-icons/fa";
-import { FaChevronUp } from "react-icons/fa";
-import { TbTriangleFilled } from "react-icons/tb";
-import { TbTriangleInvertedFilled } from "react-icons/tb";
 import { ContentQuiz, AnswerQuiz, UpdateQuiz } from "~/lib/apis/quiz";
-import QuizModal from "../../components/QuizModal";
-import QuizAnswerModal from "../../components/QuizAnswerModal";
+import QuizModal from "~/components/QuizModal";
+import QuizAnswerModal from "~/components/QuizAnswerModal";
+import StockDropdown from "~/components/StockDropdown";
+import UpDownDropdown from "~/components/UpDownDropdown";
 
 const Quiz = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isUpDownOpen, setIsUpDownOpen] = useState(false);
     const [stockName, setStockName] = useState("주식 이름");
     const [stockId, setStockId] = useState(0);
     const [date, setDate] = useState("2024-06-18");
-    const [upDown, setUpDown] = useState(true); //상승이면 true
+    const [upDown, setUpDown] = useState(true); // 상승이면 true
     const [quizNews, setQuizNews] = useState("");
     const [modalSee, setModalSee] = useState(false);
     const [answerModalSee, setAnswerModalSee] = useState(false);
@@ -28,15 +24,7 @@ const Quiz = () => {
         yesterdayCost: 0,
     });
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const upDownToggleDropdown = () => {
-        setIsUpDownOpen(!isUpDownOpen);
-    };
-
-    //date 설정 -> 오늘 날짜로 설정하기(서울 기준)
+    // date 설정 -> 오늘 날짜로 설정하기(서울 기준)
     useEffect(() => {
         const currentDate = moment().tz("Asia/Seoul").format("YYYY-MM-DD");
         setDate(currentDate);
@@ -76,14 +64,14 @@ const Quiz = () => {
     };
 
     return (
-        <div className="bg-background-pattern bg-cover bg-center h-screen">
+        <div className="bg-background-pattern bg-cover bg-center h-screen ">
             <Navbar />
-            <div className="h-300px relative flex items-center">
-                <div className="w-full h-[300px] pl-50 bg-contain bg-center bg-[url('https://stockforest.s3.ap-northeast-2.amazonaws.com/quiz/quiz_back.png')] bg-no-repeat">
-                    <div className="relative w-1/2 top-[100px] left-[20vw] ">
+            <div className="flex justify-center">
+                <div className="w-[1034px] h-[315px] mt-[50px] pl-50 bg-[url('imgs/quiz_back.png')] flex justify-center items-center scale-[0.8] m:scale-[0.9] l:scale-[1]">
+                    <div className="w-2/3 relative right-[60px]">
                         <div>{truncateText(quizNews, 150)}</div>
                         <button
-                            className="absolute right-0 text-center text-sky-400 flex items-center justify-evenly"
+                            className="flex ml-auto text-sky-400"
                             onClick={() => setModalSee(true)}
                         >
                             더보기
@@ -91,204 +79,31 @@ const Quiz = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row">
-                <div className="basis-1/3 flex justify-end relative">
-                    <div className="relative px-1">
+            <div className="flex justify-center mt-4">
+                <div className="flex items-center gap-4">
+                    <StockDropdown
+                        stockName={stockName}
+                        setStockName={setStockName}
+                        setStockId={setStockId}
+                    />
+                    <UpDownDropdown upDown={upDown} setUpDown={setUpDown} />
+                    <div className="flex justify-center w-full">
                         <button
-                            type="button"
-                            className="inline-flex items-center justify-between w-60 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            onClick={toggleDropdown}
-                        >
-                            {stockName}
-                            {isOpen ? <FaChevronDown /> : <FaChevronUp />}
-                        </button>
-
-                        {isOpen && (
-                            <div className="origin-top-right absolute right-0 top-full mt-0 w-60 max-h-60 overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                                <div
-                                    className="py-1"
-                                    role="menu"
-                                    aria-orientation="vertical"
-                                    aria-labelledby="options-menu"
-                                >
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(1);
-                                            setStockName("삼성전자");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        삼성전자
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(2);
-                                            setStockName("카카오");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        카카오
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(3);
-                                            setStockName("SM");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        SM
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(4);
-                                            setStockName("현대차");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        현대차
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(5);
-                                            setStockName("셀트리온");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        셀트리온
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(6);
-                                            setStockName("GS건설");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        GS건설
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(7);
-                                            setStockName("아모레퍼시픽");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        아모레퍼시픽
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(8);
-                                            setStockName("신라호텔");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        신라호텔
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setStockId(9);
-                                            setStockName("LG화학");
-                                            toggleDropdown();
-                                        }}
-                                    >
-                                        LG화학
-                                    </a>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className="flex basis-1/3 items-center">
-                    <div className="relative flex basis-1/2 justify-between px-1">
-                        <button
-                            type="button"
-                            className="inline-flex items-center justify-between w-60 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            onClick={upDownToggleDropdown}
-                        >
-                            {upDown ? "상승" : "하락"}
-                            {isUpDownOpen ? <FaChevronDown /> : <FaChevronUp />}
-                        </button>
-
-                        {isUpDownOpen && (
-                            <div className="origin-top-right absolute right-0 top-full mt-0 w-60 max-h-60 overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                                <div
-                                    className="py-1"
-                                    role="menu"
-                                    aria-orientation="vertical"
-                                    aria-labelledby="options-menu"
-                                >
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setUpDown(1);
-                                            upDownToggleDropdown();
-                                        }}
-                                    >
-                                        <div className="flex text-center items-center justify-evenly">
-                                            <TbTriangleFilled />
-                                            상승
-                                        </div>
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem"
-                                        onClick={() => {
-                                            setUpDown(0);
-                                            upDownToggleDropdown();
-                                        }}
-                                    >
-                                        <div className="flex text-center items-center justify-evenly">
-                                            <TbTriangleInvertedFilled />
-                                            하락
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                    <div className="basis-1/2 flex justify-center">
-                        <button
-                            className="w-40 text-center"
+                            className="w-40 text-center bg-black text-white px-4 py-2 rounded-md"
                             onClick={postAnswer}
                         >
                             저장
                         </button>
                     </div>
                 </div>
-                <div className="basis-1/3 flex justify-start">
-                    <button className="text-center" onClick={checkAnswer}>
-                        어제 푼 퀴즈 확인하기
-                    </button>
-                </div>
+            </div>
+            <div className="flex justify-center w-full mt-5">
+                <button
+                    className="text-center bg-yellow-500 text-white px-4 py-2 rounded-md"
+                    onClick={checkAnswer}
+                >
+                    어제 푼 퀴즈 확인하기
+                </button>
             </div>
             {modalSee && (
                 <QuizModal
