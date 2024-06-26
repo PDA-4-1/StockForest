db9ade1e280da952c2d94870fe14027c1f601978
hyuna <kook980316@gmail.com>
2024-06-26 10:18:24 +0900

    feat: 폭죽 효과 추가

--- front/src/routes/main/Ending.jsx
+++ front/src/routes/main/Ending.jsx
@@ -5,6 +5,8 @@ import { GetRanking } from "../../lib/apis/stock";
 import { GetUserProfile, PatchEnding } from "../../lib/apis/user";
 import { useNavigate } from "react-router-dom";
 import { saveUser } from "../../store/userSlice";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 export default function Ending() {
     const returns = useSelector((state) => state.user.user.user_returns);
@@ -22,6 +24,7 @@ export default function Ending() {
         4: "https://stockforest.s3.ap-northeast-2.amazonaws.com/profile_img/sol.png",
     };
 
+    // 새로 시작하기
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const restart = () => {
@@ -39,6 +42,10 @@ export default function Ending() {
             .catch((err) => console.log(err));
     };
 
+    // 폭죽 관련
+    const { width, height } = useWindowSize();
+
+    // 유저 랭킹 정보 불러오기
     useEffect(() => {
         GetRanking()
             .then((data) => {
@@ -50,6 +57,7 @@ export default function Ending() {
     return (
         <div className="bg-background-pattern bg-cover bg-center h-screen">
             <Navbar />
+            <Confetti width={width} height={height} gravity={0.05} numberOfPieces={150} />
             <div className="w-full h-[calc(100%_-_69.6px)] flex justify-center items-center">
                 <div className="w-[400px] min-h-[400px] h-fit bg-modal-yellow rounded-3xl grid p-6 justify-items-center gap-6">
                     <img src={imgs[myInfo.img]} className="w-40 h-40 rounded-full bg-white object-contain" />
