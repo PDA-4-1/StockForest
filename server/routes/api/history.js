b2c4f7cc8ff938d42dc04e12cd60c594bb81fea2
LimSeHyeon <dlatpgus1205@naver.com>
2024-06-26 23:16:10 +0900

    feat: 매수매도 내역 전체조회

--- server/routes/api/history.js
+++ server/routes/api/history.js
@@ -1,13 +1,34 @@
 var express = require("express");
+var pool = require("../../config/db.connect.js");
 var router = express.Router();
 
-router.get('/:stockId/:action', async (req, res) => {
+router.get("/:stockId/:action", async (req, res) => {
     // TODO : 종목별(혹은 전체), 매도/매수(혹은 전체) 조회 + 수익 계산
     const userId = req.userId;
     const stockId = req.params.stockId; // 전체는 0
-    const action = req.query.action; // 전체는 0 매수 1 매도 2
+    const action = req.params.action; // 전체는 0 매수 1 매도 2
 
-
+    try {
+        // 전체 조회
+        const allQuery = `SELECT is_buy, price, quantity, turn FROM stock_history WHERE user_id = ?`;
+        if (stockId == 0) {
+            const [allResult] = await pool.query(allQuery, [userId]);
+            console.log(allResult);
+            if (allResult.length == 0) {
+                res.send({
+                    code: 0,
+                });
+            } else {
+                res.send({
+                    code: 1,
+                    result: allResult,
+                });
+            }
+        }
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
 });
 
 module.exports = router;
