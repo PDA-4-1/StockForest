c3d3aafe38d23b69ed7ab9381a94390d1e8d285c
hyuna <kook980316@gmail.com>
2024-06-27 10:48:48 +0900

    feat: 튜토리얼 추가

--- front/src/routes/main/Tutorial.jsx
+++ front/src/routes/main/Tutorial.jsx
@@ -24,7 +24,7 @@ export default function Tutorial() {
     // 다음 단계
     const nextStep = () => {
         // 마지막 튜토리얼이면 tutorial 1로 바꾸고 마켓으로 이동
-        if (step === 20) {
+        if (step === 21) {
             PostTutorial()
                 .then((data) => {
                     console.log(data);
@@ -38,21 +38,42 @@ export default function Tutorial() {
         console.log(step);
     };
 
+    const generateImageUrls = (count) => {
+        const urls = {};
+        for (let i = 1; i <= count; i++) {
+            urls[
+                i
+            ] = `https://stockforest.s3.ap-northeast-2.amazonaws.com/tutorial/%ED%8A%9C%ED%86%A0%EB%A6%AC%EC%96%BC${i}.png`;
+        }
+        return urls;
+    };
+
+    const imgs = generateImageUrls(21);
+
     return (
         <div className="w-full h-screen max-h-screen bg-background-pattern relative grid justify-items-center">
-            <img src={logo} alt="로고" className="max-w-[500px]" />
-            <button className={`${common} animate__animated animate__heartBeat animate__infinite`}>
-                튜토리얼 시작
-            </button>
-            <img src={heart} alt="하트" className="max-w-[300px]" />
-            <button className={`${common} absolute bottom-6 left-6`} onClick={skipTutorial}>
+            {step != 0 ? (
+                <>
+                    <button className={`${common} absolute right-6 top-6`} onClick={nextStep}>
+                        {step === 20 ? "시작하기" : "다음으로"}
+                    </button>
+                    <img src={imgs[step]} className="max-h-screen w-full h-full object-fill" />
+                </>
+            ) : (
+                <>
+                    <img src={logo} alt="로고" className="max-w-[500px]" />
+                    <button
+                        className={`${common} animate__animated animate__heartBeat animate__infinite`}
+                        onClick={() => setStep(1)}
+                    >
+                        튜토리얼 시작
+                    </button>
+                    <img src={heart} alt="하트" className="max-w-[300px]" />
+                </>
+            )}
+            <button className={`${common} absolute top-6 left-6`} onClick={skipTutorial}>
                 스킵하기
             </button>
-            {step != 0 && (
-                <button className={`${common} absolute`} onClick={nextStep}>
-                    다음
-                </button>
-            )}
         </div>
     );
 }
