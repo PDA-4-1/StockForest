8f45adb05714b1453888491374bc507a6c0cd6e6
Yoon <93638922+Yoon-97@users.noreply.github.com>
2024-06-27 15:07:26 +0900

    fix: vite.config.js host 수정

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
-        host: 0,0,0,0,
+        host: '0.0.0.0',
         hmr: {
             protocol: 'ws',
-            host: 'http://stockforest.kro.kr',
+            host: 'stockforest.kro.kr',
             clientPort: 80,
+        },
     },
-    }, 
     resolve: {
         alias: [
             // 절대경로로 접근하기
