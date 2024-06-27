1b38e3c58d66381d0b3015ed0cbae02dd6ba2338
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-27 19:37:06 +0900

    update: 로그아웃 라이브러리 추가

--- front/src/components/LogoutButton.jsx
+++ front/src/components/LogoutButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import { Toast } from "./Toast";
 import { IoLogOutOutline } from "react-icons/io5";
 
@@ -8,21 +9,34 @@ const LogoutButton = () => {
     const navigate = useNavigate();
 
     const handleLogout = async () => {
-        try {
-            const response = await axios.get("api/users/logout");
-            if (response.status === 200) {
-                Toast.fire("로그아웃 되었습니다!", "", "success");
-                navigate("/login");
+        const result = await Swal.fire({
+            title: '로그아웃하시겠습니까?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: '로그아웃',
+            cancelButtonText: '취소'
+        });
+
+        if (result.isConfirmed) {
+            try {
+                const response = await axios.get("api/users/logout");
+                if (response.status === 200) {
+                    Toast.fire({
+                        icon: "success",
+                        title: "로그아웃 되었습니다!"
+                    });
+                    navigate("/login");
+                }
+            } catch (error) {
+                console.error("Logout failed:", error);
+                alert("로그아웃 실패: " + error.message);
             }
-        } catch (error) {
-            console.error("Logout failed:", error);
-            alert("로그아웃 실패: " + error.message);
         }
     };
 
     return (
         <IoLogOutOutline
-            className="text-black-500 cursor-pointer w-6 h-6 "
+            className="text-black-500 cursor-pointer w-6 h-6"
             onClick={handleLogout}
         />
     );
