f33ff3d2dadf12e1be46193564726556fada19f4
hyuna <kook980316@gmail.com>
2024-06-27 16:07:06 +0900

    chore: proxy 설정

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+        host: "0.0.0.0",
+        proxy: {
+            "/api": {
+                target: "http://localhost:4000",
+            },
+        },
     },
     resolve: {
         alias: [
